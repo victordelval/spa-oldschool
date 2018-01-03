c32437d234cff70f4b2c601edccd5d07d311eef3
victordelval <victor.val.andres@gmail.com>
2018-01-03 14:22:09 +0100

    Add requiere and some libs and initialization flow

---
+++ app/app.js
@@ -0,0 +1,52 @@
+/**
+ * RequireJS configuration of the application.
+ */
+
+requirejs.config({
+
+        baseUrl: "./",
+
+        shim: {
+            "bootstrap": {
+                "deps": [ "jquery" ]
+            }
+        },
+
+        paths: {
+            // Core libs
+            "text":             "../lib/text",
+            "jquery":           "../lib/jquery-3.2.1",
+            "bootstrap":        "../lib/bootstrap.bundle.4.0.0-beta.2",
+            "handlebars":       "../lib/handlebars-v4.0.10",
+            "director":         "../lib/director",
+            "pubsub":           "../lib/pubsub",
+
+            // Components libs
+            // "pace":             "lib/components/pace/pace.min",
+            "alertify":         "../lib/alertify.min",
+            // "spin":             "lib/components/spinjs/spin",
+            // "leaflet":          "lib/components/leaflet-1.0.3/leaflet",
+            // "leafletDraw":      "lib/components/leaflet-draw-0.3/leaflet.draw",
+
+
+            // Application scripts
+            "setup":            "app/core/setup",
+            // "core":             "spa/core",
+            // "managers":         "spa/managers",
+            // "models":           "spa/models",
+            // "views":            "spa/views",
+
+        },
+
+
+    });
+
+
+
+    /**
+     * Starting point of the application.
+     */
+
+    require(['setup'], function (setup) {
+        setup.run();
+    });
---
+++ app/core/setup.js
@@ -0,0 +1,60 @@
+/**
+ * App setup and initializer.
+ * @module setup
+ */
+
+define([
+    // "pace",
+    "jquery",
+    "alertify",
+    // "managers/navigator",
+    // "core/apiAjax"
+
+], function ($, alertify) {
+// ], function (pace, $, alertify, navigator, apiAjax) {
+
+    "use strict"
+
+    var state;
+    // var config;
+
+    /**
+     * Resolve the application config and initialize managers passing the corresponding config.
+     */
+    function init() {
+        if (state === "running") return;
+        console.log(">>>> Setup >> init ")
+        _checkUserIsLogged().then(function (data) {
+            console.log(">>>> Setup >> running ")
+            state = "running";
+            if (data.user) alertify.success('You are logged in!');
+            else alertify.error('You are not logged!');
+            // navigator.init(data);
+            // apiAjax.init();
+        });
+    }
+
+    /**
+     * Auxiliar private to check auth via ajax
+     */
+    function _checkUserIsLogged() {
+        return $.ajax({
+            type: 'GET',
+            url: "../data/user.json",
+            error: function (data) {
+                alertify.error('Error in Ajax request to check if user is logged!');
+            }
+        });
+
+
+    }
+
+
+    /**
+     * Module api
+     */
+    return {
+        run: init
+    }
+
+});
